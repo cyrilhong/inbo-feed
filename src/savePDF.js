f32bc6d02e26b646053151b2a437aefdf6fc8b49
cyrilhong2 <cyrilhong@chiuchau.com>
2024-07-31 23:29:58 +0800

    add process tag

--- src/savePDF.js
+++ src/savePDF.js
@@ -1,24 +1,26 @@
-import html2canvas from 'html2canvas'
-import jsPDF from 'jspdf'
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
 
 export default async function downloadComponentInPDF(Component) {
-  await html2canvas(Component).then((canvas) => {
-    const componentWidth = Component.offsetWidth
-    const componentHeight = Component.offsetHeight
+  const scale = 3; // 增加此值以提高解析度
+  await html2canvas(Component, { scale }).then((canvas) => {
+    const componentWidth = Component.offsetWidth * scale;
+    const componentHeight = Component.offsetHeight * scale;
 
-    const orientation = componentWidth >= componentHeight ? 'l' : 'p'
+    const orientation = componentWidth >= componentHeight ? 'l' : 'p';
 
-    const imgData = canvas.toDataURL('image/png')
+    // 使用 JPEG 格式，質量設為 0.7
+    const imgData = canvas.toDataURL('image/jpeg', 0.7);
     const pdf = new jsPDF({
       orientation,
       unit: 'px'
-    })
+    });
 
-    pdf.internal.pageSize.width = componentWidth
-    pdf.internal.pageSize.height = componentHeight
+    pdf.internal.pageSize.width = componentWidth;
+    pdf.internal.pageSize.height = componentHeight;
 
-    pdf.addImage(imgData, 'PNG', 0, 0, componentWidth, componentHeight)
+    pdf.addImage(imgData, 'JPEG', 0, 0, componentWidth, componentHeight);
     
-    pdf.save('download.pdf')
-  })
+    pdf.save('download.pdf');
+  });
 }
